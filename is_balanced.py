6985c814a9f0c4896cdc56c8f567e4d88e6ebd4b
Oscar Alejandro Sierra Valdez <Oscar_sierra_02@hotmail.com>
2018-09-10 10:50:54 -0400

    Add files via upload

---
+++ is_balanced.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 09:49:37 2018
+
+@author: oscar
+"""
+
+"""
+    Checks if the input has balanced parenthesis, brackets and square brackets
+"""
+def is_balanced(string):
+    arr = list(string)
+    stack = []
+    dict = {
+        "(" : ")",
+        "[" : "]",
+        "{" : "}"
+    } 
+    for el in arr:
+        print(stack)
+        if not stack:
+            stack.append(el)
+        else:
+            if(dict[stack[-1]] == el):
+                stack.pop()
+            else:
+                if el in stack:
+                    stack.append(el)
+                else:
+                    return False
+    print(stack)
+    if not stack:
+        return True
+    else: 
+        return False
+    
