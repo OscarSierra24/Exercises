09ea347f77452de5b31ae6f752b2fa160e22bef6
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-02-27 01:06:18 -0500

    added the book advices to is_unique

--- Strings/is_unique.py
+++ Strings/is_unique.py
@@ -1,10 +1,13 @@
 
 #Implement an algorithm to determine if a string has all unique characters. What if you
-#cannot use additional data structures?
+#cannot use additional data structures? - bit vector
 
 def is_unique(text):
     charFlag = [0] * 128
     len_text = len(text)
+
+    if len_text > 128: #if text is bigger than the possible chars it is impossible for it to have all unique chars 
+        return False 
 
     for i in range(len_text):
         char_position = ord(text[i])
