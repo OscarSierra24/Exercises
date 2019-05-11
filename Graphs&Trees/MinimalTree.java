51bac3d1fa4662b1542d518870c9f692ff011ca0
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-05-11 11:08:54 -0500

    added info

--- Graphs&Trees/MinimalTree.java
+++ Graphs&Trees/MinimalTree.java
@@ -1,3 +1,7 @@
+/*
+ * Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an
+ * algorithm to create a binary search tree with minimal height. 
+ */
 class MinimalTree {
 
   class Tree {
