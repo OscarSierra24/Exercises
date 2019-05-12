5931167c3e54fdd96620c8709e836a942c30759d
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-05-12 14:04:36 -0500

    btTree

---
+++ Graphs&Trees/BtTree.java
@@ -0,0 +1,53 @@
+class BtTree {
+  BtNode root;
+
+  public BtTree() {
+    this.root = null;
+  }
+
+  public BtTree(int value) {
+    this.root = new BtNode(value);
+  }
+
+  public void add(int value) {
+    BtNode newNode = new BtNode(value);
+    BtNode current = this.root;
+
+    if (current == null) {
+      this.root = newNode;
+      return;
+    }
+
+    while (current != null) {
+      if (newNode.value > current.value) {
+        if (current.right != null) {
+          current = current.right;
+        } else {
+          current.right = newNode;
+          break;
+        }
+      } else {
+        if (current.left != null) {
+          current = current.left;
+        } else {
+          current.left = newNode;
+          break;
+        }
+      }
+    }
+  }
+
+  private void printInorder(BtNode node) {
+    if (node == null) {
+      return;
+    }
+
+    printInorder(node.left);
+    System.out.println(node.value);
+    printInorder(node.right);
+  }
+
+  public void printInorder(){
+    printInorder(this.root);
+  }
+}
