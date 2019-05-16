95266eb3e74af3dad5803edb93aeed9892dce0e3
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-05-16 08:36:22 -0500

    added bisiesto problem

---
+++ Miscelaneous/Bisiesto.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+
+public class Bisiesto {
+ 
+  public static boolean isBisiesto(int n) {
+    
+    return (n % 4 != 0) ? false : ((n % 100 != 0) ? true : ((n % 400 == 0) ? true : false)); 
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    for (int i = 0; i < n; i++){
+      String res = isBisiesto(in.nextInt()) ? "S" : "N";
+      System.out.println(res);
+    }
+    in.close();
+  }   
+}
