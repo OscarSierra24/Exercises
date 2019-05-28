384ce509530c31cc2ea5d0cf8a77b3a1d6855d28
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-05-28 10:05:01 -0500

    deleted unnecesary pointers

--- LinkedLists/RearrangeLastNNodes.java
+++ LinkedLists/RearrangeLastNNodes.java
@@ -11,42 +11,38 @@ class RearrangeLastNNodes {
   // }
   //
   ListNode<Integer> rearrangeLastN(ListNode<Integer> l, int n) {
-    if (n == 0) return l;
-    if (l == null) return null;
+    if (n == 0 || l == null) return l;
 
-    ListNode<Integer> head; // stores the new head of the linked list
-    ListNode<Integer> current; // used to iterate the list
-    ListNode<Integer> guideNode; // the node that is n positions ahead of current
+    ListNode<Integer> slow; // used to iterate the list
+    ListNode<Integer> fast; // the node that is n positions ahead of current
     ListNode<Integer> prev;
 
     prev = null;
-    current = l;
-    guideNode = l;
+    slow = l;
+    fast = l;
 
     for (int i = 1; i < n; i++) {
-      guideNode = guideNode.next;
-      if (guideNode == null) return l; // if list length is less than n, then there's nothing to rearrange
+      fast = fast.next;
+      if (fast == null) return l; // if list length is less than n, then there's nothing to rearrange
     }
-    if (guideNode.next == null) return l; // if list length is n, then there's nothing to rearrange
+    if (fast.next == null) return l; // if list length is n, then there's nothing to rearrange
 
     // iterate until the node that is n positions ahead faces null
     // curent will be n nodes away from the end
-    while (guideNode.next != null) {
-      guideNode = guideNode.next;
-      prev = current;
-      current = current.next;
+    while (fast.next != null) {
+      fast = fast.next;
+      prev = slow;
+      slow = slow.next;
     }
     // n+1 nodes away from the end is going to be the end of the ll, and it is
     // stored on prev
     // therefore, prev next value goes to null
     prev.next = null;
-    // head is now at n nodes away from the end
-    head = current;
 
     // the tail now points towards the beggining of l
-    guideNode.next = l;
+    fast.next = l;
 
-    return head;
+    return slow;
   }
 
 }
