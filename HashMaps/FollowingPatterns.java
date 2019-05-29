c2f42dd16386c0ee81de36d1737ce0be7a6db8be
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-05-28 21:02:01 -0500

    followingpatterns

---
+++ HashMaps/FollowingPatterns.java
@@ -0,0 +1,25 @@
+class FollowingPatterns {
+  boolean areFollowingPatterns(String[] strings, String[] patterns) {
+    HashMap<String, String> map = new HashMap<>();
+    Set<String> usedPatterns = new HashSet<>();
+
+    for (int i = 0; i < strings.length; i++) {
+      if (map.containsKey(strings[i])) {
+        // if string is already in map and the patterns do not match return false
+        if (!map.get(strings[i]).equals(patterns[i])) {
+          return false;
+        }
+      } else {
+        // if the patter has already been used by another string return false
+        if (usedPatterns.contains(patterns[i])) {
+          return false;
+        }
+        usedPatterns.add(patterns[i]);
+        map.put(strings[i], patterns[i]);
+      }
+    }
+
+    return true;
+  }
+
+}
