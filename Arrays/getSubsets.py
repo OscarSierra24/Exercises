433b26e4cdff6386419df75d46c3a99a7edd82b3
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-09-05 20:35:03 -0500

    getSubsets recursive

--- Arrays/getSubsets.py
+++ Arrays/getSubsets.py
@@ -6,7 +6,7 @@ Created on Thu Sep  5 19:36:34 2019
 @author: oscar
 """
 
-def getSubsets(arr):
+def getSubsetsIt(arr):
     l = len(arr)
     combinations = pow(2, l)
     print("combinations:", combinations)
@@ -18,7 +18,28 @@ def getSubsets(arr):
                 subset.append(arr[i])
         
         print(subset)
+
+
+def _get_subsets_rec(arr, n):
+    if len(arr) == n:
+        return [[]]
+    
+    all_subsets = _get_subsets_rec(arr, n + 1)
+    current_subset = []
+    current_subsets = []
+    val = arr[n]
+    for subset in all_subsets:
+        current_subset = list(subset)
+        current_subset.append(val)
+        current_subsets.append(current_subset)
         
+    all_subsets.extend(current_subsets)
+    return all_subsets
+    
+
+def get_subsets_rec(arr): 
+    return _get_subsets_rec(arr, 0)
     
     
-getSubsets([1,2,3,4,5,6,7,8])
+    
+print(get_subsets_rec([1,2,3,4]))
